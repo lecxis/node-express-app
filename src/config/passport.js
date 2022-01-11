6fc4c2e78af60da01f73cf52737789d51deb044e
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2022-01-11 16:48:24 +0100

    creating a user

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,8 +1,8 @@
-const passport = require ('passport');
+const passport = require('passport');
 
 require('./strategies/local.strategy')();
 
-module.export = function passportConfig(app){
+module.exports = function passportConfig(app){
     app.use(passport.initialize());
     app.use(passport.session());
 
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const res = require('express/lib/response');
 const debug = require('debug')('app:authRouter')
 const {MongoClient, ObjectID} = require('mongodb');
 
 const authRouter = express.Router();
 
 authRouter.route('/signUp').post((req, res)=>{
-    res.json(req.body);
+    // create user
+    const {username, password}=req.body;
+    const url = 'mongodb+srv://dbuser:passmongo@lecximantic.pssx7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const dbName ='lecximantic';
+(async function addUser(){
+    let client;
+    try{
+        client =await MongoClient.connect(url);
+        debug('connected to the mongo DB');
+        console.log('connected to the cloud db');
+
+        const db =client.db(dbName);
+        const user= {username, password};
+        const results= await db.collection('users').insertOne(user);
+        debug(user);
+        req.login(results.ops[0], ()=>{
+            res.redirect('/auth/profile');
+        })
+        //const response =await db.collection('sessions').insertMany(sessions);
+
+    }catch(error){
+        debug(error);
+    }
+    client.close();
+    }())
+
+    
 })
 
+authRouter.route('/profile').get((req, res)=>{
+    res.json(req.user);
+})
 module.exports= authRouter;
